d7c38c4d3d3dcd1edce27f3554fc9a14022d975b
Stephan MAZUR <stephan.mazur5@gmail.com>
2018-10-11 11:27:18 +0200

    changement navbar

--- WebApplication6/Controllers/ExportToCSVController.cs
+++ WebApplication6/Controllers/ExportToCSVController.cs
@@ -90,25 +90,6 @@ namespace WebApplication6.Controllers
 
                             ));
                     }
-
-                 /*   sw.WriteLine(string.Format("\"{0}\";\"{1}\";\"{2}\";\"{3}\";\"{4}\";\"{5}\";\"{6}\";\"{7}\";\"{8}\";\"{9}\";\"{10}\";\"{11}\";\"{12}\"",
-
-                    
-                    commande.AF_RefFourniss,
-                    commande.DL_Design,
-                    commande.DL_Qte,
-                    commande.DL_PrixUnitaire,
-                    infoclient.SOCIETE_LIVRAISON,
-                    infoclient.NOM_LIVRAISON,
-                    infoclient.PRENOM_LIVRAISON,
-                    infoclient.EMAIL_LIVRAISON,
-                    infoclient.ADRESSE_LIVRAISON,
-                    infoclient.TELEPHONE_LIVRAISON,
-                    infoclient.TELEPHONE_FACTURATION,
-                    infoclient.CODE_POSTAL_LIVRAISON,
-                    infoclient.VILLE_LIVRAISON 
-
-                    ));*/
                 }
             }
             Response.ContentEncoding = Encoding.Unicode;
@@ -116,10 +97,7 @@ namespace WebApplication6.Controllers
             Response.End();
         }
 
-
-
-
-
+        
         public void GenererTousLesCSV()
 
         {
@@ -212,6 +190,97 @@ namespace WebApplication6.Controllers
             return;
             
         }
+
+
+        public void GenererLesCsvParFournisseur()
+        {
+            ListeCommandesEntities DB = new ListeCommandesEntities();
+            var fournisseurs = DB.F_DOCENTETE.Where(d => d.DO_Domaine == 1 && d.DO_Type == 12 && !d.DO_Ref.Equals("") && d.DO_Date == DateTime.Today).ToList();
+
+            foreach (var fourn in fournisseurs)
+            {
+                //string newFile = @"C:\CSV\" + fourn.DO_Tiers + " " + fourn.DO_Date.ToString().Remove(10, 9).Replace("/", "") + ".csv";
+                string newFile = @"C:\Users\SM\source\Portail fournisseur Willemse France\CSV\" + fourn.DO_Tiers + " " + fourn.DO_Date.ToString().Remove(10, 9).Replace("/", "") + ".csv";
+
+                Response.ClearContent();
+
+                StreamWriter sw = new StreamWriter(new FileStream(newFile, FileMode.Create, FileAccess.ReadWrite), Encoding.UTF8);
+
+                sw.WriteLine("\"Référence\";\"Désignation\";\"Quantité\";\"Prix unitaire\";\"Société\";\"Nom\";\"Email\";\"Adresse Livraison\";\"Complément adresse\";\"Téléphone\";\"Téléphone2\";\"Code postal\";\"Ville\"");
+
+                var commandesDuJourDocentete = DB.F_DOCENTETE.Where(d => d.DO_Domaine == 1 && d.DO_Type == 12 && !d.DO_Ref.Equals("") && d.DO_Date == DateTime.Today && d.DO_Tiers == fourn.DO_Tiers).ToList();
+
+                foreach (var commande in commandesDuJourDocentete)
+                {
+
+
+
+                    // données des produits
+                    var F_DOCLIGNE = DB.F_DOCLIGNE.Where(ligne => ligne.DO_Piece.Contains(commande.DO_Piece)).ToList();
+                    foreach (var ligne in F_DOCLIGNE)
+                    {
+                        if (String.IsNullOrEmpty(commande.POINT_RETRAIT_RELAIS))
+                        {
+                            sw.WriteLine(string.Format("\"{0}\";\"{1}\";\"{2}\";\"{3}\";\"{4}\";\"{5}\";\"{6}\";\"{7}\";\"{8}\";\"{9}\";\"{10}\";\"{11}\";\"{12}\"",
+
+
+                                ligne.AF_RefFourniss,
+                                ligne.DL_Design,
+                                ligne.DL_Qte,
+                                ligne.DL_PrixUnitaire,
+                                commande.SOCIETE_LIVRAISON,
+                                commande.CIVILITE_LIVRAISON + " " + commande.NOM_LIVRAISON + " " + commande.PRENOM_LIVRAISON,
+                                commande.EMAIL_LIVRAISON,
+                                commande.ADRESSE_LIVRAISON,
+                                commande.COMPLEMENT_LIVRAISON,
+                                commande.TELEPHONE_LIVRAISON,
+                                commande.TELEPHONE_FACTURATION,
+                                commande.CODE_POSTAL_LIVRAISON,
+                                commande.VILLE_LIVRAISON
+
+
+                                ));
+                        }
+                        else
+                        {
+
+                            sw.WriteLine(string.Format("\"{0}\";\"{1}\";\"{2}\";\"{3}\";\"{4}\";\"{5}\";\"{6}\";\"{7}\";\"{8}\";\"{9}\";\"{10}\";\"{11}\";\"{12}\"",
+
+                            ligne.AF_RefFourniss,
+                            ligne.DL_Design,
+                            ligne.DL_Qte,
+                            ligne.DL_PrixUnitaire,
+                            commande.SOCIETE_FACTURATION,
+                            commande.CIVILITE_FACTURATION + " " + commande.NOM_FACTURATION + " " + commande.PRENOM_FACTURATION,
+                            commande.EMAIL_FACTURATION,
+                            commande.ADRESSE_FACTURATION,
+                            commande.COMPLEMENT_FACTURATION,
+                            commande.TELEPHONE_LIVRAISON,
+                            commande.TELEPHONE_FACTURATION,
+                            commande.CODE_POSTAL_FACTURATION,
+                            commande.VILLE_FACTURATION
+
+                                ));
+                        }
+
+                    };
+
+
+
+
+                }
+                Response.Write(sw.ToString());
+                Response.End();
+
+                sw.Close();
+
+            }
+
+
+
+        }
+
     }
 }
+
     
